e5a1a4d2478b98ffe185bbbd840681ad110fef21
luiscerquone <lcerquone@steplix.com>
2024-03-18 14:11:22 -0300

    deleted unused import

--- androidApp/src/main/java/com/example/startedproject/android/screens/ArticlesScreen.kt
+++ androidApp/src/main/java/com/example/startedproject/android/screens/ArticlesScreen.kt
@@ -8,16 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Info
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
